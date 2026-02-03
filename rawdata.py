d04dcc8894d1490c94d18ff4a0dce288e17d7f79
Priyanka16R <rpriyankar1604@gmail.com>
2026-02-03 15:29:02 +0530

    SECOND COMMIT

---
+++ rawdata.py
@@ -0,0 +1,8 @@
+item_name=str("MacBook")
+quantity=int(4)
+price=float(45.7)
+in_stock=True
+print("Item:",item_name ,", Quantity:",quantity,", price:",price,", Avaliable:",in_stock)
+total=quantity*price
+print("total cost:",total)
+
