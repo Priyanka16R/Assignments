f7af02905338860efb47c44a9b51d78c8de1b9bd
Priyanka16R <rpriyankar1604@gmail.com>
2026-02-03 12:55:05 +0530

    First commit

---
+++ bill.py
@@ -0,0 +1,6 @@
+print("Welcome to Famous Restrorent")
+total_bill=float(input("entre the Total Bill Amount:"))
+total_no=int(input("enter the total number of persons:"))
+share_per_person=total_bill/total_no
+print("Total Bill :",total_bill,".Each person pays :",share_per_person)
+print(type(total_bill))
